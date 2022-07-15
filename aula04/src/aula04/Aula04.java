7dcacf6ec5f1fc35a8cf60056d29f15ef84a7524
JulielJ <julieljunior@hotmail.com>
2022-07-15 08:04:27 -0300

    Add files via upload

---
+++ aula04/src/aula04/Aula04.java
@@ -0,0 +1,17 @@
+package aula04;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		
+		Caneta c1 = new Caneta("PIC", 1.0f, "azul");
+		//c1.setModelo("BIC");
+	//	c1.setPonta(0.5f);
+		//c1.status();
+	
+	
+	Caneta c2 = new Caneta("FC", 0.5f, "Vermelha");
+	//	c2.status();
+	
+}
+}
